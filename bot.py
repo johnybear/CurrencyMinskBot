2b5d5725e9d2d92f0d06381ad66e114fa0fa448f
johnybear <johnybear3@outlook.com>
2017-06-24 18:58:55 +0300

    added some things

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import telegram
 from telegram.ext import CommandHandler, Updater, RegexHandler, MessageHandler, ConversationHandler, Filters
 import logging
+from currency_parser import currency_response
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger('bot')
@@ -9,42 +10,54 @@ logger = logging.getLogger('bot')
 class FindBankBot:
     START_COMMAND = 'start'
     CURRENCIES = ['USD', 'EUR', 'RUB']
-    OPERATIONS = ['SELL', 'BUY']
-    SEND_LOCATION_MESSAGE = 'SEND LOCATION'
+    OPERATIONS = ['Банки продают', 'Банки покупают']
+    SEND_LOCATION_MESSAGE = 'Вышлите местоположение'
+    RESTART_TEXT = 'Сбросить все и начать сначала'
 
     CURRENCIES_BUTTONS = [telegram.KeyboardButton(text=currency, callback_data=currency) for currency in CURRENCIES]
     OPERATIONS_BUTTONS = [telegram.KeyboardButton(text=operation, callback_data=operation) for operation in OPERATIONS]
     LOCATION_BUTTON = telegram.KeyboardButton(text=SEND_LOCATION_MESSAGE, request_location=True)
+    RESTART_BUTTON = telegram.KeyboardButton(text=RESTART_TEXT, callback_data=RESTART_TEXT)
 
-    CURRENCY_KEYBOARD = telegram.ReplyKeyboardMarkup([[button for button in CURRENCIES_BUTTONS]], resize_keyboard=True, one_time_keyboard=True)
-    OPERATIONS_KEYBOARD = telegram.ReplyKeyboardMarkup([[button for button in OPERATIONS_BUTTONS]], resize_keyboard=True, one_time_keyboard=True)
-    LOCATION_KEYBOARD = telegram.ReplyKeyboardMarkup([[LOCATION_BUTTON]], resize_keyboard=True, one_time_keyboard=True)
+    CURRENCY_KEYBOARD = telegram.ReplyKeyboardMarkup(
+        [[button for button in CURRENCIES_BUTTONS], [RESTART_BUTTON]], resize_keyboard=True,)
+    OPERATIONS_KEYBOARD = telegram.ReplyKeyboardMarkup(
+        [[button for button in OPERATIONS_BUTTONS], [RESTART_BUTTON]], resize_keyboard=True,)
+    LOCATION_KEYBOARD = telegram.ReplyKeyboardMarkup(
+        [[LOCATION_BUTTON], [RESTART_BUTTON]], resize_keyboard=True,)
 
     OPERATION_TYPE, CURRENCY, LOCATION = range(3)
 
     def __init__(self):
-        self.updater = Updater(token='445708182:AAEsrNtCjd0LWgJtTiKuSolgamftU5WnCpU', workers=1000)
+        self.updater = Updater(token='***', workers=1000)
         self.dispatcher = self.updater.dispatcher
         self.__add_handlers()
 
     def __add_handlers(self):
         logger.info('add handlers')
         conv_handler = ConversationHandler(
-            entry_points=[CommandHandler(self.START_COMMAND, self.start)],
+            entry_points=[CommandHandler(self.START_COMMAND, self.start),
+            			  RegexHandler('', self.start)],
 
             states={
-                self.OPERATION_TYPE: [RegexHandler('^(SELL|BUY)$', self.operation, pass_user_data=True)],
+                self.OPERATION_TYPE: [RegexHandler('^(Банки продают|Банки покупают)$', self.operation, pass_user_data=True),
+                    				  RegexHandler('^('+self.RESTART_TEXT+')$', self.start),
+	                                  RegexHandler('', self.default_response)],
 
-                self.CURRENCY: [RegexHandler('^(USD|EUR|RUB)$', self.currency, pass_user_data=True)],
+                self.CURRENCY: [RegexHandler('^(USD|EUR|RUB)$', self.currency, pass_user_data=True),
+                                RegexHandler('^('+self.RESTART_TEXT+')$', self.start),
+                                RegexHandler('', self.default_response)],
 
-                self.LOCATION: [MessageHandler(Filters.location, self.location, pass_user_data=True)]
+                self.LOCATION: [MessageHandler(Filters.location, self.location, pass_user_data=True), 
+                                RegexHandler('^('+self.RESTART_TEXT+')$', self.start),
+                                RegexHandler('', self.default_response)],
 
             },
 
             fallbacks=[RegexHandler('^Show$', self.done, pass_user_data=True)]
         )
         self.dispatcher.add_handler(conv_handler)
-        self.dispatcher.add_handler(RegexHandler('', self.default_response))
+        #self.dispatcher.add_handler(RegexHandler('', self.default_response))
         logger.info('handlers successfuly added')
 
     def send_message(self, bot, update, message, keyboard):
@@ -56,43 +69,55 @@ class FindBankBot:
                 reply_markup=keyboard,
                 parse_mode=telegram.ParseMode.HTML)
         except Exception as e:
-            logger.info(f'Message not sent: {e}')
+            logger.info('Message not sent: %s' % e)
 
     def default_response(self, bot, update):
-        self.send_message(bot, update, message=f'Unknown command', keyboard=self.CURRENCY_KEYBOARD)
+        self.send_message(
+            bot, 
+            update, 
+            message='Небольшой сбой в работе.',
+            keyboard=self.OPERATION_TYPE)
+        return self.OPERATION_TYPE
+ 
 
     def currency(self, bot, update, user_data):
         text = update.message.text
         user_data['choise']['currency'] = text
-        update.message.reply_text('Now send your location to find banks near you!', reply_markup=self.LOCATION_KEYBOARD)
+        update.message.reply_text(
+            'Теперь вышлите свое местоположение, чтоб найти ближайшие к вам обменники! GPS должен быть включен', 
+            reply_markup=self.LOCATION_KEYBOARD)
         return self.LOCATION
 
     def location(self, bot, update, user_data):
         user = update.message.from_user
+        text = update.message.text
         user_location = update.message.location
-        user_data['choise']['location'] = user_location
-        text_to_log = f'Location of {user.first_name}: {user_location.latitude} / {user_location.longitude}\n' \
-                      f'Operation type "{user_data["choise"]["operation"]}"\n' \
-                      f'Currency "{user_data["choise"]["currency"]}"'
-        logger.info(text_to_log)
-        update.message.reply_text(text_to_log)
+        user_data['choise']['user_location'] = user_location
+        # text_to_log = f'Location of {user.first_name}: {user_location.latitude} / {user_location.longitude}\n' \
+        #               f'Operation type "{user_data["choise"]["operation"]}"\n' \
+        #               f'Currency "{user_data["choise"]["currency"]}"'
+        response = currency_response(**user_data['choise'])
+        #logger.info(text_to_log)
+        update.message.reply_text(response, reply_markup=self.OPERATIONS_KEYBOARD)
+        return self.OPERATION_TYPE
 
     def done(self, bot, update):
         pass
 
     def operation(self, bot, update, user_data):
+        operation_dict = {'Банки продают':"sell", 'Банки покупают':"buy"}
         text = update.message.text
         user_data['choise'] = {}
-        user_data['choise']['operation'] = text
-        update.message.reply_text('Now choose a currency you want to exchange', reply_markup=self.CURRENCY_KEYBOARD)
+        user_data['choise']['operation'] = operation_dict[text]
+        update.message.reply_text('Выберите валюту, которую хотели бы поменять.', reply_markup=self.CURRENCY_KEYBOARD)
         return self.CURRENCY
 
     def start(self, bot, update):
         logger.info('Start command')
         user = update.message.from_user
         self.send_message(bot, update,
-                          message=f'<b>Hello, {user.first_name}!</b>\n'
-                                  f'Choose a type of operation', keyboard=self.OPERATIONS_KEYBOARD)
+                          message='<b>Здравствуйте, %s!</b>\n Выберите тип операции' % user.first_name, 
+                          keyboard=self.OPERATIONS_KEYBOARD)
         return self.OPERATION_TYPE
 
     def polling(self):
--- currency_parser.py
+++ currency_parser.py
@@ -4,54 +4,71 @@ from bs4 import BeautifulSoup
 import requests
 
 CURRENCY_INDEX = {
-	("buy", "usd"):0,
-	("sell", "usd"):1,
-	("buy", "eur"):2,
-	("sell", "eur"):3,
-	("buy", "rub"):4,
-	("sell", "rub"):5
+    ("buy", "USD"):0,
+    ("sell", "USD"):1,
+    ("buy", "EUR"):2,
+    ("sell", "EUR"):3,
+    ("buy", "RUB"):4,
+    ("sell", "RUB"):5
 }
+
+user_requests = {}
 
 page_html = requests.get("https://myfin.by/currency/minsk").text 
 SOUP_PAGE = BeautifulSoup(page_html, 'html.parser')
 with open("bank_locations.json", "r") as b:
-	BANK_LOCATIONS= json.load(b)
+    BANK_LOCATIONS= json.load(b)
+
+def distance(user_location, bank_location):
+    b_lat, b_long = bank_location["latitude"], bank_location["longitude"]
+    u_lat, u_long = user_location.latitude, user_location.longitude
+    lon1, lat1, lon2, lat2 = map(radians, [b_long, b_lat, u_long, u_lat])
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    km = 6367 * c
+    return km
 
 
 def within_km(user_location, bank_location):
-	b_lat, b_long = tuple(map(float, bank_location))
-	u_lat, u_long = user_location.latitude, user_location.longitude
-	lon1, lat1, lon2, lat2 = map(radians, [b_long, b_lat, u_long, u_lat])
-	# haversine formula 
-	dlon = lon2 - lon1 
-	dlat = lat2 - lat1 
-	a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-	c = 2 * asin(sqrt(a)) 
-	km = 6367 * c
-	return km <= 1
+    km = distance(user_location, bank_location)
+    return km <= 1
 
 def course_info(bank, operation, curr):
-	c_index = CURRENCY_INDEX[(operation, curr)]
-	title = bank.select("div.ttl > a")[0].getText()
-	phone = bank.select("div.tel")[0].getText()
-	address = bank.select("div.address")[0].getText()
-	course = "курс обмена: " + bank.select("td > span.first_curr")[c_index].getText()
-	return [title, phone, address, course]
+    c_index = CURRENCY_INDEX[(operation, curr)]
+    if curr=="RUB":
+        curr="100 RUB"
+    else:
+        curr="1 "+curr
+    title = bank.select("div.ttl > a")[0].getText()
+    phone = bank.select("div.tel")[0].getText()
+    address = bank.select("div.address")[0].getText()
+    course = "%s за %s BYN" % (curr,
+                           bank.select("td > span.first_curr")[c_index].getText())
+    return [title, phone, address, course]
 
 
 def stringify_response(curr_list, operation):
-	response = sorted(curr_list, key=lambda x: x[3])
-	if operation == "buy":
-		response.reverse()
-	response = map("\n".join, response)
-	response = map(lambda x: x.strip(), response)#remove empty values from each elem
-	response = ("\n"+(("*")*26)+"\n").join(response)
-	return response
+    response = sorted(curr_list, key=lambda x: x[3])
+    if operation == "buy":
+        response.reverse()
+    response = map("\n".join, response)
+    response = map(lambda x: x.strip(), response)#remove empty values from each elem
+    response = ("\n"+(("*")*26)+"\n").join(response)
+    operation = "БАНКИ ПРОДАЮТ\n\n" if operation=='sell' else "БАНКИ ПОКУПАЮТ\n\n"
+    response = operation+response
+    return response
 
 
-def currency_response(user_location, operation, curr):
-	closest_banks = [ad for ad, loc in BANK_LOCATIONS.items() if within_km(user_location, BANK_LOCATIONS[ad])]
-	banks = SOUP_PAGE.select("tr.currency_row_1")
-	unsorted_curr_list = [course_info(b, operation, curr) for b in banks if b.select("div.address")[0].getText() in closest_banks]
-	response = stringify_response(unsorted_curr_list, operation)
-	return response
+def currency_response(user_location, operation, currency):
+    closest_banks = [ad for ad, loc in BANK_LOCATIONS.items() if within_km(user_location, loc)]
+    if not closest_banks:
+        distances = ((ad, distance(user_location, loc)) for ad, loc in BANK_LOCATIONS.items())
+        distances = sorted(distances, key=lambda x: x[1])[0:3]
+        closest_banks = [ad[0] for ad in distances]
+    banks = SOUP_PAGE.select("tr.currency_row_1")
+    unsorted_curr_list = [course_info(b, operation, currency) for b in banks if b.select("div.address")[0].getText() in closest_banks]
+    response = stringify_response(unsorted_curr_list, operation)
+    return response
